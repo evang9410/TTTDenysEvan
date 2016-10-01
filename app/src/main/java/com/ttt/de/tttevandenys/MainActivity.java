73d0758a99afa584ae630af4a638eca8cc199faf
evan.glicakis <evang9410@gmail.com>
2016-10-01 14:42:13 -0400

    Added comments Added aletdialog for winners added strings to go with these alert dialogs

--- app/src/main/java/com/ttt/de/tttevandenys/MainActivity.java
+++ app/src/main/java/com/ttt/de/tttevandenys/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ttt.de.tttevandenys;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +13,10 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.w3c.dom.Text;
 
@@ -36,18 +43,23 @@ public class MainActivity extends AppCompatActivity {
 
     // SharedPreferences for saving to disk
     private SharedPreferences prefs;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        prefs = getSharedPreferences("scores",MODE_PRIVATE);
+        prefs = getSharedPreferences("scores", MODE_PRIVATE);
         //private method to instantiate the view objects in this activity.
         load_gui();
         // load board from SharedPreferences
-        if(savedInstanceState != null){
-            if(savedInstanceState.containsKey("board_state"))
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey("board_state"))
                 load_board_state(savedInstanceState);
         }
 
@@ -71,11 +83,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 reset_board();
 
-                if(isDroid) {
+                if (isDroid) {
                     isDroid = false;
                     tv_playing.setText(R.string.playing_player1);
-                }
-                else {
+                } else {
                     isDroid = true;
                     tv_playing.setText(R.string.playing_droid);
                 }
@@ -99,6 +110,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * onClickListener for the "About" button.
+         * Fires the AboutActivity Intent which displays the about game and credits.
+         */
+
         btn_about.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -107,22 +123,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    
 
-    private void start_scores(){
-        Intent scores_intent = new Intent(MainActivity.this,ScoresActivity.class);
+    /**
+     * Private method used to fire the score activity
+     */
+    private void start_scores() {
+        Intent scores_intent = new Intent(MainActivity.this, ScoresActivity.class);
         startActivity(scores_intent);
     }
 
-    private void start_about(){
-        Intent about_intent = new Intent(MainActivity.this,AboutActivity.class);
+    /**
+     * Private method used to fire the about activity.
+     */
+    private void start_about() {
+        Intent about_intent = new Intent(MainActivity.this, AboutActivity.class);
         startActivity(about_intent);
     }
 
+    /**
+     * Private method used to reset the board
+     * Sets the tiles back to their original image
+     * Makes the tiles clickable again
+     * Sets the tags back to their numbered order [1-9]
+     */
     private void reset_board() {
-        for(int i =0; i<9; i++){
+        for (int i = 0; i < 9; i++) {
             board[i].setImageResource(R.drawable.tile);
             board[i].setClickable(true);
             board[i].setTag(i + 1 + "");
@@ -130,7 +160,10 @@ public class MainActivity extends AppCompatActivity {
         turn = 0;
     }
 
-    private void zero_scores(){
+    /**
+     * Private method to set the scores saved to disk to zero.
+     */
+    private void zero_scores() {
         player1_wins = 0;
         player2_wins = 0;
         droid_wins = 0;
@@ -138,74 +171,78 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences.Editor editor = prefs.edit();
         editor.putInt("player1_wins", player1_wins);
-        editor.putInt("player2_wins",player2_wins);
-        editor.putInt("droid_wins",droid_wins);
-        editor.putInt("draw_count",draw_count);
+        editor.putInt("player2_wins", player2_wins);
+        editor.putInt("droid_wins", droid_wins);
+        editor.putInt("draw_count", draw_count);
         editor.commit();
 
-        Toast toast = Toast.makeText(MainActivity.this,getText(R.string.zero_toast),Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(MainActivity.this, getText(R.string.zero_toast), Toast.LENGTH_SHORT);
         toast.show();
     }
 
-
-    public void tile_click(View v){
+    /**
+     * Main bread and butter of the app.
+     * onClickListener for every ImageButton in the MainActivity.
+     * Handles the changes of ImageButtons depending on who's pressing them
+     * Displays the winning messages and draw message incrementing the appropriate counters.
+     *
+     * @param v
+     */
+    public void tile_click(View v) {
 
         SharedPreferences.Editor editor = prefs.edit();
 
-        ImageButton ib = (ImageButton)v;
+        ImageButton ib = (ImageButton) v;
         ib.setClickable(false);
-        if(check_win().equals("none")){
-            if(turn % 2 == 0){
+        if (check_win().equals("none")) {
+            if (turn % 2 == 0) {
                 // Player1's turn
-                Log.d("Turn","player1 turn");
-                if(isDroid)
+                Log.d("Turn", "player1 turn");
+                if (isDroid)
                     tv_playing.setText(R.string.playing_droid);
                 else
                     tv_playing.setText(R.string.playing_player1);
+
                 ib.setTag("x");
                 ib.setImageResource(R.drawable.tile_x);
                 turn++;
                 check_win();
-                if(check_win().equals("none") & turn < 9) {
+                if (check_win().equals("none") & turn < 9) {
                     if (isDroid) {
                         droid_turn();
                     }
                 }
-            }
-            else{
+            } else {
                 Log.d("Turn", "player2 turn");
-                if(!isDroid)
+                if (!isDroid)
                     tv_playing.setText(R.string.playing_player1);
-
-                ib.setTag("o");
-                ib.setImageResource(R.drawable.tile_o);
-                turn++;
-                check_win();
+                if(check_win().equals("none")) {
+                    ib.setTag("o");
+                    ib.setImageResource(R.drawable.tile_o);
+                    turn++;
+                    check_win();
+                }
             }
 
-            if(check_win().equals("x")){
-                Toast toast = Toast.makeText(MainActivity.this,getText(R.string.p1_win_toast),Toast.LENGTH_SHORT);
-                toast.show();
+            if (check_win().equals("x")) {
+                alert_user(R.string.alert_message_p1);
+
                 player1_wins++;
                 editor.putInt("player1_wins", player1_wins);
                 editor.commit();
-            }
-            else if(check_win().equals("o") & isDroid){
-                Toast toast = Toast.makeText(MainActivity.this,getText(R.string.droid_wins_toast),Toast.LENGTH_SHORT);
-                toast.show();
+            } else if (check_win().equals("o") & isDroid) {
+                alert_user(R.string.alert_message_droid);
                 droid_wins++;
                 editor.putInt("droid_wins", droid_wins);
                 editor.commit();
-            }
-            else if(check_win().equals("o") & isDroid == false){
-                Toast toast = Toast.makeText(MainActivity.this,getText(R.string.p2_win_toast),Toast.LENGTH_SHORT);
-                toast.show();
+            } else if (check_win().equals("o") & isDroid == false) {
+                alert_user(R.string.alert_message_p2);
                 player2_wins++;
                 editor.putInt("player2_wins", player2_wins);
                 editor.commit();
             }
-            if(turn == 9 && check_win().equals("none")){
-                Toast toast = Toast.makeText(MainActivity.this,getText(R.string.draw_toast),Toast.LENGTH_LONG);
+            if (turn == 9 && check_win().equals("none")) {
+                Toast toast = Toast.makeText(MainActivity.this, getText(R.string.draw_toast), Toast.LENGTH_LONG);
                 toast.show();
                 draw_count++;
                 editor.putInt("draw_count", draw_count);
@@ -213,26 +250,55 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        save_board_state();
+        //save_board_state();
+    }
+
+    private void alert_user(int winner) {
+
+        // Build the alert dialog box to be showed when a player wins.
+        AlertDialog.Builder alert_dialog = new AlertDialog.Builder(MainActivity.this);
+        // set title
+        alert_dialog.setTitle(getString(R.string.alert_title));
+        // set message
+        switch (winner){
+            case R.string.alert_message_p1:
+                alert_dialog.setMessage(getString(R.string.alert_message_p1));
+                break;
+            case R.string.alert_message_p2:
+                alert_dialog.setMessage(getString(R.string.alert_message_p2));
+                break;
+            default:
+                alert_dialog.setMessage(getString(R.string.alert_message_droid));
+        }
+        alert_dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        // create alert dialog box with the builder.
+        AlertDialog alert = alert_dialog.create();
+        // show the message
+        alert.show();
+
     }
 
     /**
      * Loops through the board array (Array of tiles of the tic tac toe game)
      * and turns them into an array of strings, following the order of the tiles from 0-9
      * saves the string array to be loaded upon state change.
+     *
      * @return
      */
-    private String[] save_board_state(){
+    private String[] save_board_state() {
         String[] board_tags = new String[board.length];
-        for(int i = 0; i < board_tags.length; i++){
-            if(board[i].getTag().equals("x")){
+        for (int i = 0; i < board_tags.length; i++) {
+            if (board[i].getTag().equals("x")) {
                 board_tags[i] = "x";
-            }
-            else if(board[i].getTag().equals("o")){
+            } else if (board[i].getTag().equals("o")) {
                 board_tags[i] = "o";
-            }
-            else
-                board_tags[i]= i+1 + "";
+            } else
+                board_tags[i] = i + 1 + "";
         }
         return board_tags;
     }
@@ -240,36 +306,39 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Loads the string array that was saved when the state changed and applies the proper properties
      * to the image buttons.
+     *
      * @param b
      */
-    private void load_board_state(Bundle b){
+    private void load_board_state(Bundle b) {
         String[] arr = b.getStringArray("board_state");
 
-        for(int i = 0; i < board.length; i++){
-            if(arr[i].equals("x")){
+        for (int i = 0; i < board.length; i++) {
+            if (arr[i].equals("x")) {
                 board[i].setImageResource(R.drawable.tile_x);
                 board[i].setTag("x");
                 board[i].setClickable(false);
-            }
-            else if(arr[i].equals("o")){
+            } else if (arr[i].equals("o")) {
                 board[i].setImageResource(R.drawable.tile_o);
                 board[i].setTag("o");
                 board[i].setClickable(false);
-            }
-            else{
+            } else {
                 board[i].setImageResource(R.drawable.tile);
                 board[i].setClickable(true);
-                board[i].setTag(i+1 + "");
+                board[i].setTag(i + 1 + "");
             }
         }
     }
 
-    private void droid_turn(){
+    /**
+     * Randoms a number from 1-9 and checks if the tile coresponding to that number is a valid
+     * ImageButton that can be played. If not - it re rolls.
+     */
+    private void droid_turn() {
         Log.d("Turn", "droid turn");
         boolean play = true;
         Random rnd = new Random();
         int rng = rnd.nextInt(9);
-        if(check_win().equals("none")) {
+        if (check_win().equals("none")) {
             while (true) {
                 if (!board[rng].getTag().equals("x") & !board[rng].getTag().equals("o")) {
                     tile_click(board[rng]);
@@ -282,27 +351,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private String check_win(){
+    /**
+     * Checks if the game is over and returns a string of the tile that won.
+     * "none" means no one has won yet.
+     *
+     * @return String "x" or "o" or "none"
+     */
+    private String check_win() {
 
-        for(int i =0; i<3; i++)
-        {
+        for (int i = 0; i < 3; i++) {
             //Check horizontal lines
-            if(board[i*3].getTag().equals(board[1+i*3].getTag()) && board[i*3].getTag().equals(board[2+i*3].getTag()))
-            {
-                return board[i*3].getTag().toString();
+            if (board[i * 3].getTag().equals(board[1 + i * 3].getTag()) && board[i * 3].getTag().equals(board[2 + i * 3].getTag())) {
+                return board[i * 3].getTag().toString();
             }
 
 
             //Check vertical lines
-            else if(board[i].getTag().equals(board[3+i].getTag()) && board[i].getTag().equals(board[6+i].getTag()))
-            {
+            else if (board[i].getTag().equals(board[3 + i].getTag()) && board[i].getTag().equals(board[6 + i].getTag())) {
                 return board[i].getTag().toString();
             }
             //Check diagonals
-            else if(i < 2)
-            {
-                if(board[4].getTag().equals(board[i*2].getTag()) && board[4].getTag().equals(board[i*-2 + 8].getTag()))
-                {
+            else if (i < 2) {
+                if (board[4].getTag().equals(board[i * 2].getTag()) && board[4].getTag().equals(board[i * -2 + 8].getTag())) {
                     return board[4].getTag().toString();
                 }
             }
@@ -316,39 +386,86 @@ public class MainActivity extends AppCompatActivity {
      * Loads the ImageButtons into an array.
      * Loads the Game play buttons.
      */
-    private void load_gui(){
+    private void load_gui() {
         // Load the ImageButtons
-        board[0] = (ImageButton)findViewById(R.id.tile_1);
-        board[1] = (ImageButton)findViewById(R.id.tile_2);
-        board[2] = (ImageButton)findViewById(R.id.tile_3);
-        board[3] = (ImageButton)findViewById(R.id.tile_4);
-        board[4] = (ImageButton)findViewById(R.id.tile_5);
-        board[5] = (ImageButton)findViewById(R.id.tile_6);
-        board[6] = (ImageButton)findViewById(R.id.tile_7);
-        board[7] = (ImageButton)findViewById(R.id.tile_8);
-        board[8] = (ImageButton)findViewById(R.id.tile_9);
+        board[0] = (ImageButton) findViewById(R.id.tile_1);
+        board[1] = (ImageButton) findViewById(R.id.tile_2);
+        board[2] = (ImageButton) findViewById(R.id.tile_3);
+        board[3] = (ImageButton) findViewById(R.id.tile_4);
+        board[4] = (ImageButton) findViewById(R.id.tile_5);
+        board[5] = (ImageButton) findViewById(R.id.tile_6);
+        board[6] = (ImageButton) findViewById(R.id.tile_7);
+        board[7] = (ImageButton) findViewById(R.id.tile_8);
+        board[8] = (ImageButton) findViewById(R.id.tile_9);
 
         // Load the Buttons
-        btn_reset = (Button)findViewById(R.id.btn_reset);
-        btn_about = (Button)findViewById(R.id.btn_about);
-        btn_play = (Button)findViewById(R.id.btn_play);
-        btn_zero = (Button)findViewById(R.id.btn_zero);
-        btn_scores = (Button)findViewById(R.id.btn_scores);
+        btn_reset = (Button) findViewById(R.id.btn_reset);
+        btn_about = (Button) findViewById(R.id.btn_about);
+        btn_play = (Button) findViewById(R.id.btn_play);
+        btn_zero = (Button) findViewById(R.id.btn_zero);
+        btn_scores = (Button) findViewById(R.id.btn_scores);
 
         // Load the textview
-        tv_playing = (TextView)findViewById(R.id.tv_playing);
-        if(isDroid){
+        tv_playing = (TextView) findViewById(R.id.tv_playing);
+        if (isDroid) {
             tv_playing.setText(R.string.playing_droid);
-        }else{
+        } else {
             tv_playing.setText(R.string.playing_droid);
         }
     }
     // INSTANCE STATE STUFF
+
+    /**
+     * Saves the state of the board (which tiles have been changed) to the bundle to be loaded if the
+     * activity's state changes.
+     *
+     * @param outState
+     */
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putStringArray("board_state", save_board_state());
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ttt.de.tttevandenys/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ttt.de.tttevandenys/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
 
--- app/src/main/java/com/ttt/de/tttevandenys/ScoresActivity.java
+++ app/src/main/java/com/ttt/de/tttevandenys/ScoresActivity.java
@@ -13,12 +13,16 @@ public class ScoresActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores);
-        prefs = getSharedPreferences("scores",MODE_PRIVATE);
+        prefs = getSharedPreferences("scores",MODE_PRIVATE); // Get the SharedPreferences named "scores"
 
         load_textviews();
         load_scores();
     }
 
+    /**
+     * Loads the TextViews from the activity_scores.xml
+     * to be manipulated through java.
+     */
     private void load_textviews(){
         tv_p1_wins = (TextView)findViewById(R.id.scores_tv_player1_wins);
         tv_p2_wins = (TextView)findViewById(R.id.scores_tv_player2_wins);
@@ -26,6 +30,9 @@ public class ScoresActivity extends AppCompatActivity {
         tv_draws = (TextView)findViewById(R.id.scores_tv_draws);
     }
 
+    /**
+     * sets the TextViews to their scores that are saved on disk.
+     */
     private void load_scores(){
         tv_p1_wins.setText(String.valueOf(prefs.getInt("player1_wins",0)));
         tv_p2_wins.setText(String.valueOf(prefs.getInt("player2_wins",0)));
@@ -34,7 +41,7 @@ public class ScoresActivity extends AppCompatActivity {
     }
 
 
-
+    /*
     @Override
     protected void onSaveInstanceState(Bundle outState){
         super.onSaveInstanceState(outState);
@@ -42,5 +49,5 @@ public class ScoresActivity extends AppCompatActivity {
         outState.putInt("p2_score",prefs.getInt("player2_wins",0));
         outState.putInt("droid_score",prefs.getInt("droid_wins",0));
         outState.putInt("draws",prefs.getInt("draw_count",0));
-    }
+    }*/
 }
